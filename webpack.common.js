922d064604e7b9ffb247f617df0c3db31cf376e7
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-04-22 19:38:38 +0100

    Installation of fonts
    Added font files:
    - electrical.woff/2
    
    Update _carousel.scss:
    - updated carousel font-family
    
    Update main.scss:
    - imported fonts from resets folder
    
    Added _fonts.scss:
    - imported 'Electrical' font
    
    Updated _variables.scss:
    - now using $header-font as $electrical fallback
    
    Update dependencies:
    - installed file-loader
    
    Update webpack:
    - added test for fonts to use file-loader

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
@@ -13,6 +14,19 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              entry: path.resolve(__dirname, "inc/fonts"),
+              name: "[name].[ext]",
+              outputPath: "../fonts/"
+            }
+          }
+        ]
       }
     ]
   },
